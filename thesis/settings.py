041d573f5874c9a9b7443d7076429c1fa5c9cffc
Jan Christopher Francisco <152581275+jcfrancisco0103@users.noreply.github.com>
2025-02-13 16:54:28 +0000

    Fixed homepage UI

--- thesis/settings.py
+++ thesis/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'inventory',
     'accounts',
     'manageorders',
+    'django.contrib.staticfiles',
 ]
 
 MIDDLEWARE = [
@@ -120,10 +121,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
+
+STATIC_URL = 'accounts/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "accounts/static"),
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
